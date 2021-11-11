8707c12fbf60c83aeb72d1ae61031404f3da5a4b
SergejBeschastnov <svb19.09@yandex.ru>
2021-11-11 18:58:00 +0300

    Classes+tests done

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Bowerman } from './js/Bowerman';
+import { Character } from './js/Character';
+import { Daemon } from './js/Daemon';
+import { Magician } from './js/Magician';
+import { Swordsman } from './js/Swordsman';
+import { Undead } from './js/Undead';
+import { Zombie } from './js/Zombie';
